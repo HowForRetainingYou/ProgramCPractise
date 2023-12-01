c2ce6a9b076719a7983c2d1fba7b46cc21e2d2d2
HowForRetainingYou <3058602461@qq.com>
2023-12-01 23:00:56 +0800

    2023.12.01的一些入门基础练习

---
+++ 2023.12.01/2023.12.01/2023.12.1习题.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+////输出图案由小v组成的大V
+//int main()
+//{
+//    printf("v   v\n");
+//    printf(" v v\n");
+//    printf("  v\n");
+//    return 0;
+//
+//}
+
+
+//确定不同整型数据类型在内存中占多大（字节），输出不同整型数据类型在内存中占多大（字节）。
+//int main()
+//{
+//	printf("The size of short is %d bytes.\n", sizeof(short));
+//	printf("The size of int is %d bytes.\n", sizeof(int));
+//	printf("The size of long is %d bytes.\n", sizeof(long));
+//	printf("The size of long long is %d bytes.\n", sizeof(long));
+//	return 0;
+//}
+//输出指定图案
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("     **     \n");
+//    printf("     **     \n");
+//    printf("************\n");
+//    printf("************\n");
+//    printf("    *  *     \n");
+//    printf("    *  *     \n");
+//    return 0;
+//}
+
+
+
+//输入3科成绩，然后把三科成绩输出，成绩为整数形式。
+//#include <stdio.h>
+//int main()
+//{
+//    int a, b, c;
+//    scanf("%d %d %d", &a, &b, &c);
+//    printf("score1=%d,score2=%d,score3=%d", a, b, c);
+//    return 0;
+//}
+
+
