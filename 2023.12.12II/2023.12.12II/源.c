4a3197b00dcb0869fcc12a5f9dcda022ba23eb99
HowForRetainingYou <3058602461@qq.com>
2023-12-12 22:47:12 +0800

    练习

---
+++ 2023.12.12II/2023.12.12II/源.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int n;
+	while (scanf("%d", &n) != EOF)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+
+			for (int u = 1; u <=i/*u<=n*/; u++)
+			{
+
+
+				printf("* ");
+
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+
+}
+
+
+
+
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int n;
+//    while (scanf("%d", &n) != EOF)
+//    {
+//        for (int i = 1; i <= n; i++)
+//
+//        {
+//            for (int u = 1; u <= n; u++)
+//            {
+//                printf("* ");
+//            }
+//            printf("\n");
+//        }
+//
+//    }
+//
+//
+//    return 0;
+//}
+
