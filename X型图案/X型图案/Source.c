96393a7874763948f324269ccb2b2df443eee854
HowForRetainingYou <3058602461@qq.cpm>
2024-03-06 07:42:03 -0800

    .

---
+++ X型图案/X型图案/Source.c
@@ -0,0 +1,57 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//题目：https://www.nowcoder.com/practice/83d6afe3018e44539c51265165806ee4?tpId=107&&tqId=33349&rp=1&ru=/ta/beginner-programmers&qru=/ta/beginner-programmers/question-ranking
+
+//我的与博哥的答案本质上是一样的；
+//这道题看着虽难虽复杂，但解这道题的核心是找规律；
+//在草稿纸上画出题目图案，再标记规律
+int main()
+{
+	int x;
+	while (scanf("%d", &x) != EOF)
+	{
+		for (int i = 1; i < x + 1; i++)
+		{
+			for (int j = 1; j < x + 1; j++)
+			{
+				if (j == i || j + i == x + 1)
+				{
+					printf("*");
+				}
+				else
+				{
+					printf(" ");
+				}
+				
+			}
+			printf("\n");
+		}
+	}
+
+}
+
+
+
+//唐朝版答案；太繁琐
+int main3()
+{
+	int n, X = 0;
+	while (scanf("%d", &n) != EOF)
+	{
+		int N = n;
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = 1; j <= n; j++)
+			{
+				if (j == n - N + 1 || j == N)
+					printf("*");
+				else
+					printf(" ");
+			}
+			printf("\n");
+			N = N - 1;
+		}
+	}
+	return 0;
+}
---
+++ 最大公约数/最大公约数/Source.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//熟悉数学，利用辗转相除法求；
+int main()
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d %d", &a, &b);
+	int c=a%b;//a b顺序无关紧要，到后面是进行一样的步骤的；
+	
+	if (a!=0&&b!=0)
+	{
+		while (c != 0)
+		{
+			a = b;
+			b = c;
+			c = a % b;
+		}
+	}
+	printf("最大公约数是：%d", b);
+	return 0;
+}
+
