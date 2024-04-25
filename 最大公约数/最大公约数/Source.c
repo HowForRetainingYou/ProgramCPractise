f58fa1b1ad88b0f4d07c752651fe084e8f5cd2eb
HowForRetainingYou <3058602461@qq.cpm>
2024-04-25 08:49:21 -0700

    .

--- 最大公约数/最大公约数/Source.c
+++ 最大公约数/最大公约数/Source.c
@@ -1,14 +1,14 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <math.h>
 
 //熟悉数学，利用辗转相除法求；
 int main()
 {
 	int a = 0;
 	int b = 0;
-	scanf("%d %d", &a, &b);
-	int c=a%b;//a b顺序无关紧要，到后面是进行一样的步骤的；
-	
+	scanf("%d %d", &a, &b);//24 32
+	int c=a%b;//a b顺序无关紧要，到后面是进行一样的步骤的；//24
 	if (a!=0&&b!=0)
 	{
 		while (c != 0)
@@ -22,3 +22,16 @@ int main()
 	return 0;
 }
 
+
+
+
+
+//
+//int main()
+//{
+//	float a = 2.0
+//		;
+//	float b = 4.0;
+//	printf("%f", exp(1));
+//
+//}
